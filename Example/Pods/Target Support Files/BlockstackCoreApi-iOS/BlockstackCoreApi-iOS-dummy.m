1fc1d7e6ec56cd2db97e82cf9a0f9e9cbb9f1b98
Logan Sease <lsease@gmail.com>
2017-07-24 08:21:07 -0400

    added git ignore

--- Example/Pods/Target Support Files/BlockstackCoreApi-iOS/BlockstackCoreApi-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BlockstackCoreApi_iOS : NSObject
-@end
-@implementation PodsDummy_BlockstackCoreApi_iOS
-@end
--- Example/Pods/Target Support Files/Pods-BlockstackCoreApi_Tests/Pods-BlockstackCoreApi_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BlockstackCoreApi_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_BlockstackCoreApi_Tests
-@end

8c987932fd72ddf8884e24dbab1ad6730e68a2e0
Logan Sease <lsease@gmail.com>
2017-07-31 15:13:11 -0400

    create Auth Response object definition

--- BlockstackCoreApi/Classes/AuthDataTypes.swift
+++ BlockstackCoreApi/Classes/AuthDataTypes.swift
@@ -15,3 +15,41 @@ public struct AppManifest : Serializable
     public var themeColorHex : String = "#000000"
     public var backgroundColorHex : String = "#ffffff"
 }
+
+public struct AuthResponse : Serializable
+{
+    public var username : String?
+    public var profile : Profile
+    public var appPrivateKey : String
+    public var coreSessionToken : String
+    public var authResponseToken : String
+    
+    public struct Profile : Serializable
+    {
+        public var account : [Account] = []
+        public var givenName : String?
+        public var familyName : String?
+        public var description : String?
+        public var image : [Image] = []
+        public var address : Address?
+        
+        public struct Account : Serializable
+        {
+            public var identifier : String
+            public var service : String
+            public var contentUrl : String
+        }
+        
+        public struct Image : Serializable
+        {
+            public var name : String
+            public var contentUrl : String
+        }
+        
+        public struct Address : Serializable
+        {
+            public var addressLocality : String
+            public var addressCountry : String
+        }
+    }
+}
